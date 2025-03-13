560ac3a483f2bce8008f00b720c630323b95ce70
colaola20 <soryo@farmingdale.edu>
2025-03-13 19:02:50 -0400

    added FCFS_calculations() to  calculate wait and turnaround time for precesses

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,19 @@ public class Main {
 
 
 
-    public void FCFS_calculations(ArrayList<int[]> processes) {
-
+    public static void FCFS_calculations(List<int[]> processes) {
+        int waitingTime = 0;
+        int turnaroundTime = 0;
+        int i = 0;
+        int[] currentProcess;
+        System.out.println("-----------FCFS-----------");
+        System.out.println("Process ID | Waiting Time | Turnaround Time");
+        while (!processes.isEmpty()) {
+            currentProcess = processes.remove(i);
+            turnaroundTime = waitingTime + currentProcess[1];
+            System.out.printf("    %d      |      %d       |     %d\n", currentProcess[0], waitingTime, turnaroundTime);
+            waitingTime +=  currentProcess[1];
+        }
     }
 
     public static void main(String[] args) {
@@ -19,5 +30,7 @@ public class Main {
         processes.add(new int[] {3, 8, 4});
         processes.add(new int[] {4, 4, 2});
         processes.add(new int[] {5, 5, 3});
+
+        FCFS_calculations(processes);
     }
 }
