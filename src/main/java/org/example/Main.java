1db98aee3a4bcc57e719d5f3e89a902279dcf5c8
colaola20 <soryo@farmingdale.edu>
2025-03-13 19:11:44 -0400

    did some fixes to FCFS_calculations() method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,21 +9,29 @@ public class Main {
 
     public static void FCFS_calculations(List<int[]> processes) {
         int waitingTime = 0;
-        int turnaroundTime = 0;
+        int turnaroundTime;
         int i = 0;
         int[] currentProcess;
+        double sum = 0;
+        double amountOfProcesses = processes.size();
+        double average;
         System.out.println("-----------FCFS-----------");
         System.out.println("Process ID | Waiting Time | Turnaround Time");
         while (!processes.isEmpty()) {
-            currentProcess = processes.remove(i);
+            sum += waitingTime;
+            currentProcess = processes.remove(0);
             turnaroundTime = waitingTime + currentProcess[1];
             System.out.printf("    %d      |      %d       |     %d\n", currentProcess[0], waitingTime, turnaroundTime);
             waitingTime +=  currentProcess[1];
         }
+        average = sum / amountOfProcesses;
+        System.out.printf("\nThe average time: %.2f\n", average);
     }
 
+    //public static void
+
     public static void main(String[] args) {
-        List<int[]> processes = new ArrayList<int[]>();
+        List<int[]> processes = new ArrayList<>();
 
         processes.add(new int[] {1, 2, 2});
         processes.add(new int[] {2, 1, 1});
